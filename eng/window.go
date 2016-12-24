c94cf47b354f4ddd8bfd921c80a70767a589c9e4
Phestek <damian.koski97@gmail.com>
2016-12-24 22:15:23 +0100

    Simple windowing.

---
+++ eng/window.go
@@ -0,0 +1,40 @@
+package eng
+
+import "github.com/veandco/go-sdl2/sdl"
+
+type Window struct {
+	handle   *sdl.Window
+	renderer *sdl.Renderer
+}
+
+func NewWindow(title string, width int, height int) *Window {
+	window := new(Window)
+	var err error
+
+	window.handle, err = sdl.CreateWindow(title, sdl.WINDOWPOS_CENTERED, sdl.WINDOWPOS_CENTERED, width, height,
+		sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+
+	window.renderer, err = sdl.CreateRenderer(window.handle, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		panic(err)
+	}
+
+	return window
+}
+
+func (window *Window) Delete() {
+	window.handle.Destroy()
+	window.renderer.Destroy()
+}
+
+func (window *Window) Clear(red, green, blue, alpha uint8) {
+	window.renderer.SetDrawColor(red, green, blue, alpha)
+	window.renderer.Clear()
+}
+
+func (window *Window) Display() {
+	window.renderer.Present()
+}
--- main.go
+++ main.go
@@ -3,16 +3,26 @@ package main
 import (
 	"github.com/veandco/go-sdl2/sdl"
 	"github.com/veandco/go-sdl2/sdl_image"
+	"github.com/phestek/farming_rpg/eng"
 )
 
 func main() {
 	sdl.Init(sdl.INIT_AUDIO | sdl.INIT_TIMER | sdl.INIT_VIDEO)
 	img.Init(img.INIT_PNG)
 
+	window := eng.NewWindow("Farming RPG", 1280, 720)
+
 	running := true
 	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+			}
+		}
+		window.Clear(100, 50, 200, 255)
+		window.Display()
 	}
 
 	sdl.Quit()
 }
-
