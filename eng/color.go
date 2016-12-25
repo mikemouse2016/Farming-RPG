a9c3349ebebaca2ac766e493e2356cd6bca923fd
Phestek <damian.koski97@gmail.com>
2016-12-25 14:15:27 +0100

    Added basic const colors.

--- eng/color.go
+++ eng/color.go
@@ -6,3 +6,14 @@ type Color struct {
 	Blue  byte
 	Alpha byte
 }
+
+// Because Go doesn't support const structs, we must use a little hack (they are still not const ;_;)
+func ColorBlack() Color       { return Color{0, 0, 0, 255} }
+func ColorWhite() Color       { return Color{255, 255, 255, 255} }
+func ColorRed() Color         { return Color{255, 0, 0, 255} }
+func ColorGreen() Color       { return Color{0, 255, 0, 255 }}
+func ColorBlue() Color        { return Color{0, 0, 255, 255} }
+func ColorYellow() Color      { return Color{255, 255, 0, 255} }
+func ColorMagenta() Color     { return Color{255, 0, 255, 255} }
+func ColorCyan() Color        { return Color{0, 255, 255, 255} }
+func ColorTransparent() Color { return Color{0, 0, 0, 0} }
