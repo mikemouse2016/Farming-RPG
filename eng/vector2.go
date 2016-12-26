b5f0ae30eb3a54a0103d4debf3db0018b60b29f5
Phestek <damian.koski97@gmail.com>
2016-12-26 13:36:49 +0100

    Added more vector manipulations methods.

--- eng/vector2.go
+++ eng/vector2.go
@@ -1,13 +1,15 @@
 package eng
 
+import "math"
+
 type Vector2i struct {
 	X int
 	Y int
 }
 
 type Vector2u struct {
-	X int32
-	Y int32
+	X uint
+	Y uint
 }
 
 type Vector2f struct {
@@ -23,6 +25,22 @@ func (vector Vector2i) Sub(other Vector2i) Vector2i {
 	return Vector2i{X: vector.X - other.X, Y: vector.Y - other.Y}
 }
 
+func (vector Vector2i) Mul(other Vector2i) Vector2i {
+	return Vector2i{X: vector.X * other.X, Y: vector.Y * other.Y}
+}
+
+func (vector Vector2i) MulByValue(value int) Vector2i {
+	return Vector2i{X: vector.X * value, Y: vector.Y * value}
+}
+
+func (vector Vector2i) Div(other Vector2i) Vector2i {
+	return Vector2i{X: vector.X / other.X, Y: vector.Y / other.Y}
+}
+
+func (vector Vector2i) DivByValue(value int) Vector2i {
+	return Vector2i{X: vector.X / value, Y: vector.Y / value}
+}
+
 func (vector Vector2u) Add(other Vector2u) Vector2u {
 	return Vector2u{vector.X + other.X, vector.Y + other.Y}
 }
@@ -31,10 +49,53 @@ func (vector Vector2u) Sub(other Vector2u) Vector2u {
 	return Vector2u{vector.X - other.X, vector.Y - other.Y}
 }
 
+func (vector Vector2u) Mul(other Vector2u) Vector2u {
+	return Vector2u{X: vector.X * other.X, Y: vector.Y * other.Y}
+}
+
+func (vector Vector2u) MulByValue(value uint) Vector2u {
+	return Vector2u{X: vector.X * value, Y: vector.Y * value}
+}
+
+func (vector Vector2u) Div(other Vector2u) Vector2u {
+	return Vector2u{X: vector.X / other.X, Y: vector.Y / other.Y}
+}
+
+func (vector Vector2u) DivByValue(value uint) Vector2u {
+	return Vector2u{X: vector.X / value, Y: vector.Y / value}
+}
+
 func (vector Vector2f) Add(other Vector2f) Vector2f {
 	return Vector2f{vector.X + other.X, vector.Y + other.Y}
 }
 
 func (vector Vector2f) Sub(other Vector2f) Vector2f {
 	return Vector2f{vector.X - other.X, vector.Y - other.Y}
+}
+
+func (vector Vector2f) Mul(other Vector2f) Vector2f {
+	return Vector2f{X: vector.X * other.X, Y: vector.Y * other.Y}
+}
+
+func (vector Vector2f) MulByValue(value float32) Vector2f {
+	return Vector2f{X: vector.X * value, Y: vector.Y * value}
+}
+
+func (vector Vector2f) Div(other Vector2f) Vector2f {
+	return Vector2f{X: vector.X / other.X, Y: vector.Y / other.Y}
+}
+
+func (vector Vector2f) DivByValue(value float32) Vector2f {
+	return Vector2f{X: vector.X / value, Y: vector.Y / value}
+}
+
+func (vector Vector2f) Length() float32 {
+	length2 := math.Pow(float64(vector.X), 2) + math.Pow(float64(vector.Y), 2)
+	length := float32(math.Sqrt(length2))
+	return length
+}
+
+func (vector Vector2f) Normalize() Vector2f {
+	length := vector.Length()
+	return Vector2f{X: vector.X / length, Y: vector.Y / length}
 }
--- eng/vector2_test.go
+++ eng/vector2_test.go
@@ -22,6 +22,44 @@ func TestVector2i_Sub(t *testing.T) {
 	}
 }
 
+func TestVector2i_Mul(t *testing.T) {
+	vec1 := Vector2i{-123, 432}
+	vec2 := Vector2i{5, 7}
+	vec3 := vec1.Mul(vec2)
+	vec3Expected := Vector2i{-615, 3024}
+	if vec3.X != vec3Expected.X || vec3.Y != vec3Expected.Y {
+		t.Error("Expected", vec3Expected, "got", vec3)
+	}
+}
+
+func TestVector2i_MulByValue(t *testing.T) {
+	vec1 := Vector2i{-123, 432}
+	vec3 := vec1.MulByValue(4)
+	vec3Expected := Vector2i{-492, 1728}
+	if vec3.X != vec3Expected.X || vec3.Y != vec3Expected.Y {
+		t.Error("Expected", vec3Expected, "got", vec3)
+	}
+}
+
+func TestVector2i_Div(t *testing.T) {
+	vec1 := Vector2i{-2226, 632}
+	vec2 := Vector2i{432, 523}
+	vec3 := vec1.Div(vec2)
+	vec3Expected := Vector2i{-5, 1}
+	if vec3.X != vec3Expected.X || vec3.Y != vec3Expected.Y {
+		t.Error("Expected", vec3Expected, "got", vec3)
+	}
+}
+
+func TestVector2i_DivByValue(t *testing.T) {
+	vec1 := Vector2i{-123, 432}
+	vec3 := vec1.DivByValue(3)
+	vec3Expected := Vector2i{-41, 144}
+	if vec3.X != vec3Expected.X || vec3.Y != vec3Expected.Y {
+		t.Error("Expected", vec3Expected, "got", vec3)
+	}
+}
+
 func TestVector2u_Add(t *testing.T) {
 	vec1 := Vector2i{324, 432}
 	vec2 := Vector2i{432, 523}
