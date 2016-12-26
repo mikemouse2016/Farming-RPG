d3686efaadc505a5d68e36e3743a52c3b8b65459
Phestek <damian.koski97@gmail.com>
2016-12-26 19:14:09 +0100

    Added SDL_mixer initialization.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/veandco/go-sdl2/sdl_image"
 
 	"github.com/phestek/farming_rpg/core"
+	"github.com/veandco/go-sdl2/sdl_mixer"
 )
 
 func init() {
@@ -16,6 +17,7 @@ func init() {
 func main() {
 	sdl.Init(sdl.INIT_AUDIO | sdl.INIT_TIMER | sdl.INIT_VIDEO)
 	img.Init(img.INIT_PNG)
+	mix.Init(mix.INIT_OGG | mix.INIT_MP3)
 
 	var game core.Game
 	game.Init()
